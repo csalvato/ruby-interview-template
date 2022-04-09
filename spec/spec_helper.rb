f9ecb0e1ac737e52140d59eae610affcff58e5a9
Chris Salvato <csalvato@gmail.com>
2022-04-09 09:38:44 -0600

    Enables focused specs and --only-failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,7 +46,7 @@ RSpec.configure do |config|
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
-=begin
+
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
@@ -57,8 +57,9 @@ RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.example_status_persistence_file_path = 'tmp/examples.txt'
 
+=begin
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
   # https://relishapp.com/rspec/rspec-core/docs/configuration/zero-monkey-patching-mode
